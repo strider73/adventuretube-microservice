3a486e2f1b7669aebab4c1de55e167d110c1519a
strider73 <strider.lee@gmail.com>
2024-05-19 01:17:48 +1000

    project initialized using a Spring initializer

---
+++ src/test/java/com/adventuretube/adventuretubemicroservice/AdventuretubeMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adventuretube.adventuretubemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventuretubeMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
