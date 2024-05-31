279a34094cc4b4a0996c33e9cbf81e8b6d8b46d8
strider73 <strider.lee@gmail.com>
2024-06-01 00:19:23 +1000

    Class ApiGateWayApplication change to GatewayApplication

--- gateway-service/src/main/java/com/adventuretube/apigateway/GatewayApplication.java
+++ gateway-service/src/main/java/com/adventuretube/apigateway/GatewayApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiGatewayApplication {
+public class GatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayApplication.class);
+        SpringApplication.run(GatewayApplication.class);
     }
 }
