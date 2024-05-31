419bc79b9f43707d8cdf20642861763751224181
strider73 <strider.lee@gmail.com>
2024-06-01 03:55:40 +1000

    SecurityServiceConfig has removed all security configuration but restTemplate  to create @Bean

--- security-service/src/main/java/com/adventuretube/security/SecurityServiceConfig.java
+++ security-service/src/main/java/com/adventuretube/security/SecurityServiceConfig.java
@@ -3,48 +3,48 @@ package com.adventuretube.security;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.client.RestTemplate;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+//
+//import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
 public class SecurityServiceConfig {
 
 
-    @Bean
-    @Order(1)
-    public SecurityFilterChain apiFilterChain(HttpSecurity httpSecurity) throws  Exception{
-    httpSecurity
-            .securityMatcher("/api/**")
-            .authorizeHttpRequests(authorize -> authorize
-                    .anyRequest().hasRole("ADMIN")
-            )
-            .httpBasic(withDefaults());
-
-     return httpSecurity.build();
-
-    }
-
-    @Bean
-    @Order(1)
-    public SecurityFilterChain formLoginFilterChain(HttpSecurity httpSecurity) throws  Exception{
-        httpSecurity
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().authenticated()
-                )
-                .formLogin(withDefaults());
-
-        return httpSecurity.build();
-
-    }
+//    @Bean
+//    @Order(1)
+//    public SecurityFilterChain apiFilterChain(HttpSecurity httpSecurity) throws  Exception{
+//    httpSecurity
+//            .securityMatcher("/api/**")
+//            .authorizeHttpRequests(authorize -> authorize
+//                    .anyRequest().hasRole("ADMIN")
+//            )
+//            .httpBasic(withDefaults());
+//
+//     return httpSecurity.build();
+//
+//    }
+//
+//    @Bean
+//    @Order(1)
+//    public SecurityFilterChain formLoginFilterChain(HttpSecurity httpSecurity) throws  Exception{
+//        httpSecurity
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .anyRequest().authenticated()
+//                )
+//                .formLogin(withDefaults());
+//
+//        return httpSecurity.build();
+//
+//    }
 
     @Bean
     // after create eureka server without @LoadBalanced  annotation  restTemplate call
