97376f03d1fdf6f8bd570850939d43aab6c8b131
strider73 <strider.lee@gmail.com>
2024-06-03 23:31:05 +1000

    Create Test Call in geospatial

---
+++ geospatial-service/src/main/java/com/adventuretube/geospatial/controller/AdventureTubeDataController.java
@@ -0,0 +1,17 @@
+package com.adventuretube.geospatial.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/geo")
+public class AdventureTubeDataController {
+
+    @GetMapping("/save")
+    public ResponseEntity<String> save(){
+        return ResponseEntity.ok("Your AdventuretubeData has been saved");
+    }
+}
