87fa6d8e5e8d8bbda2e94c8c5bace7434de389c9
strider73 <strider.lee@gmail.com>
2024-06-03 23:34:38 +1000

    Add security-dependency on security-service

--- security-service/src/main/java/com/adventuretube/security/SecurityServiceConfig.java
+++ security-service/src/main/java/com/adventuretube/security/SecurityServiceConfig.java
@@ -10,28 +10,36 @@ import org.springframework.context.annotation.Configuration;
 //import org.springframework.security.core.userdetails.UserDetailsService;
 //import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 //import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.client.RestTemplate;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 //
 //import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-//@EnableWebSecurity
+@EnableWebSecurity
 public class SecurityServiceConfig {
 
 
-//    @Bean
-//    @Order(1)
-//    public SecurityFilterChain apiFilterChain(HttpSecurity httpSecurity) throws  Exception{
-//    httpSecurity
-//            .securityMatcher("/api/**")
-//            .authorizeHttpRequests(authorize -> authorize
-//                    .anyRequest().hasRole("ADMIN")
-//            )
-//            .httpBasic(withDefaults());
-//
-//     return httpSecurity.build();
-//
-//    }
+    @Bean
+    @Order(1)
+    public SecurityFilterChain apiFilterChain(HttpSecurity httpSecurity) throws  Exception{
+    httpSecurity
+            .securityMatcher("/auth/**")
+            .authorizeHttpRequests(authorize -> authorize
+                    .requestMatchers("/auth/register").permitAll()
+                    .anyRequest().hasRole("ADMIN")
+            )
+            .csrf(csrf -> csrf.ignoringRequestMatchers("/auth/register")) // Disable CSRF for /auth/register
+            .httpBasic(withDefaults());
+
+     return httpSecurity.build();
+
+    }
 //
 //    @Bean
 //    @Order(1)
