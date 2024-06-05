288f5e4c5c99c21bc89398608ed240e18da5dc8b
strider73 <strider.lee@gmail.com>
2024-06-06 01:58:02 +1000

    Add Spring-security for geospatial-service
       Spring-security now has been added
       for Authorization at
        web-service
        member-service
        geospatial-service
    
        Authentication and JWT token validation
        gateway-service

---
+++ geospatial-service/src/main/java/com/adventuretube/geospatial/GeospatialServiceConfig.java
@@ -0,0 +1,35 @@
+package com.adventuretube.geospatial;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class GeospatialServiceConfig {
+
+//    @Bean
+//    public RestTemplate restTemplate(){
+//          return new RestTemplate();
+//    }
+
+
+    @Bean
+    public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
+            http.csrf().disable()//RestAPI with JWT doen't require csrf protection
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/security/**").hasRole("ADMIN")  // Restrict /security/** to ADMIN role
+                        .anyRequest().permitAll()  // Allow all other requests
+                )
+//                .csrf(csrf -> csrf.ignoringRequestMatchers("/member/**")) // Disable CSRF for /auth/register
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+
+
+
+
+}
