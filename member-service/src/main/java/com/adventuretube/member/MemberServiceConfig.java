7c279cfa9b6072a725b16273814c416d77722fb2
strider73 <strider.lee@gmail.com>
2024-06-05 23:43:53 +1000

    Remove all Security from member-service   1 remove spring-boot-starter-security dependency   2 remove UserDetail implements from security package   3 remove security configuration on the MemberServiceConfig

--- member-service/src/main/java/com/adventuretube/member/MemberServiceConfig.java
+++ member-service/src/main/java/com/adventuretube/member/MemberServiceConfig.java
@@ -1,11 +1,7 @@
 package com.adventuretube.member;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class MemberServiceConfig {
@@ -16,18 +12,18 @@ public class MemberServiceConfig {
 //    }
 
 
-    @Bean
-    public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/security/**").hasRole("ADMIN")  // Restrict /security/** to ADMIN role
-                        .anyRequest().permitAll()  // Allow all other requests
-                )
-                .csrf(csrf -> csrf.ignoringRequestMatchers("/member/**")) // Disable CSRF for /auth/register
-                .httpBasic(withDefaults());
-        return http.build();
-    }
-
+//    @Bean
+//    public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers("/security/**").hasRole("ADMIN")  // Restrict /security/** to ADMIN role
+//                        .anyRequest().permitAll()  // Allow all other requests
+//                )
+//                .csrf(csrf -> csrf.ignoringRequestMatchers("/member/**")) // Disable CSRF for /auth/register
+//                .httpBasic(withDefaults());
+//        return http.build();
+//    }
+//
 
 
 
--- member-service/src/main/java/com/adventuretube/member/model/Member.java
+++ member-service/src/main/java/com/adventuretube/member/model/Member.java
@@ -7,20 +7,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.Collection;
+
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Member implements UserDetails {
+public class Member {
     @Id
     @SequenceGenerator(
             name = "member_sequence",
@@ -40,29 +37,4 @@ public class Member implements UserDetails {
     private String role;
     private LocalDateTime createAt;
 
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
