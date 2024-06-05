01a3df7f6498821502f12047e861cb123d6ac937
strider73 <strider.lee@gmail.com>
2024-06-05 18:26:09 +1000

    Add GlobalExceptionHandler

--- security-service/src/main/java/com/adventuretube/security/exceptions/GlobalExceptionHandler.java
+++ security-service/src/main/java/com/adventuretube/security/exceptions/GlobalExceptionHandler.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -19,5 +21,18 @@ public class GlobalExceptionHandler {
                 System.currentTimeMillis()
         );
         return new ResponseEntity<>(errorResponse,HttpStatus.CONFLICT);
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleUnknownException(Exception ex) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                ex.getMessage(),
+                "Internal Server Error",
+                INTERNAL_SERVER_ERROR.value(),
+                System.currentTimeMillis()
+           );
+
+            return new ResponseEntity<>(errorResponse, INTERNAL_SERVER_ERROR);
     }
 }
