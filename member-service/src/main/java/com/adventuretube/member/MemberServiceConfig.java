4b8c25f55ba0f99afbced8d57038585c6ad2f58e
strider73 <strider.lee@gmail.com>
2024-06-07 03:30:35 +1000

    Add all security setting for security-service

--- member-service/src/main/java/com/adventuretube/member/MemberServiceConfig.java
+++ member-service/src/main/java/com/adventuretube/member/MemberServiceConfig.java
@@ -1,7 +1,11 @@
 package com.adventuretube.member;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class MemberServiceConfig {
@@ -12,18 +16,18 @@ public class MemberServiceConfig {
 //    }
 
 
-//    @Bean
-//    public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests(authorize -> authorize
-//                        .requestMatchers("/security/**").hasRole("ADMIN")  // Restrict /security/** to ADMIN role
-//                        .anyRequest().permitAll()  // Allow all other requests
-//                )
+    @Bean
+    public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
+            http.csrf().disable()//RestAPI with JWT doen't require csrf protection
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/security/**").hasRole("ADMIN")  // Restrict /security/** to ADMIN role
+                        .anyRequest().permitAll()  // Allow all other requests
+                )
 //                .csrf(csrf -> csrf.ignoringRequestMatchers("/member/**")) // Disable CSRF for /auth/register
-//                .httpBasic(withDefaults());
-//        return http.build();
-//    }
-//
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
 
 
 
--- member-service/src/main/java/com/adventuretube/member/model/Member.java
+++ member-service/src/main/java/com/adventuretube/member/model/Member.java
@@ -7,17 +7,20 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
-
+import java.util.Arrays;
+import java.util.Collection;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Member {
+public class Member implements UserDetails {
     @Id
     @SequenceGenerator(
             name = "member_sequence",
@@ -37,4 +40,29 @@ public class Member {
     private String role;
     private LocalDateTime createAt;
 
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
