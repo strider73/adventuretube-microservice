1c196f52a06b4cf9d1d50fda212f76037caaa86c
strider73 <strider.lee@gmail.com>
2024-06-07 03:39:02 +1000

    DTO does not get into any further than  Controller layer

--- member-service/src/main/java/com/adventuretube/member/controller/MemberController.java
+++ member-service/src/main/java/com/adventuretube/member/controller/MemberController.java
@@ -5,8 +5,10 @@ import com.adventuretube.member.model.Member;
 import com.adventuretube.member.service.MemberService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Slf4j
@@ -18,7 +20,16 @@ public class MemberController {
     @PostMapping("registerMember")
     public UserDTO registerMember(@RequestBody UserDTO userDTO){
         log.info("new member registration {}",userDTO);
-        return memberService.registerMember(userDTO);
+        Member newMember = Member.builder()
+                .username(userDTO.getUsername())
+                .password(userDTO.getPassword())
+                .email(userDTO.getEmail())
+                .channeld(userDTO.getChannelId())
+                .role("USER")
+                .createAt(LocalDateTime.now())
+                .build();
+        Member registeredMember = memberService.registerMember(newMember);
+        return createUserDTO(registeredMember);
     }
 
 
@@ -31,4 +42,27 @@ public class MemberController {
         return false;
     }
 
+
+    @PostMapping("findMemberByEmail")
+    public UserDTO  findMemberByEmail(@RequestBody String email){
+        Optional<Member> member = memberService.findEmail(email);
+        if(member.isPresent()){
+            return createUserDTO(member.get());
+        }
+        return null;
+    }
+
+
+
+    //when I try to copy member to userDTO using a BeanUtils
+    //Id is not copied
+    private UserDTO createUserDTO(Member member){
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(member.getId());
+        userDTO.setPassword(member.getPassword());
+        userDTO.setEmail(member.getEmail());
+        userDTO.setPassword(member.getPassword());
+        userDTO.setRole(member.getRole());
+        return userDTO;
+    }
 }
