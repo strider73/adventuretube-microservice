d8e393d37346013615b262e816916675d9b1e84a
strider73 <strider.lee@gmail.com>
2024-06-18 23:53:34 +1000

    Nothing but format the indentation

--- security-service/src/main/java/com/adventuretube/security/service/JwtUtil.java
+++ security-service/src/main/java/com/adventuretube/security/service/JwtUtil.java
@@ -31,43 +31,38 @@ public class JwtUtil {
     private Key SECRET_KEY;
 
     @PostConstruct
-    public void initKey(){
+    public void initKey() {
         this.SECRET_KEY = Keys.hmacShaKeyFor(secret.getBytes());
         //this.SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256); // or HS384, or HS512
 
 
     }
 
-    public Claims getClaims(String token){
+    public Claims getClaims(String token) {
         try {
-        return Jwts.parser()
-                .setSigningKey(SECRET_KEY)
-                .build()
-                .parseSignedClaims(token)
-                .getPayload();
+            return Jwts.parser()
+                    .setSigningKey(SECRET_KEY)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
         } catch (SignatureException | ExpiredJwtException e) { // Invalid signature or expired token
             log.error(e.getMessage());
             throw new AccessDeniedException("Access denied: " + e.getMessage());
         }
 
     }
+
     public <T> T getClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = getClaims(token);
         return claimsResolver.apply(claims);
     }
 
-    public String getUserName(String token){
-        return getClaim(token,Claims::getSubject);
-    }
-    public Date getExpirationDate(String token) {
-        return getClaim(token , Claims::getExpiration);
-    }
 
     public String generate(String userId, String role, String tokenType) {
         Map<String, String> claims = Map.of("id", userId, "role", role);
         long expMillis = "ACCESS".equalsIgnoreCase(tokenType)
                 ? Long.parseLong(accessTokenExpiration) * 1000
-                : Long.parseLong(refreshTokenExpiration) * 1000 ;
+                : Long.parseLong(refreshTokenExpiration) * 1000;
 
         final Date now = new Date();
         final Date exp = new Date(now.getTime() + expMillis);
@@ -84,11 +79,15 @@ public class JwtUtil {
     public String extractUsername(String token) {
         return getClaim(token, Claims::getSubject);
     }
+
+    public Date getExpirationDate(String token) {
+        return getClaim(token, Claims::getExpiration);
+    }
+
     public Boolean validateToken(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername()));
     }
-
 
 
     private Key getSignKey() {
