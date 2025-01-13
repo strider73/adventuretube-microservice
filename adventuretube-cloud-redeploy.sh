d58f5193e49b40c56bfe338dfdbd3a154067d9a6
strider73 <strider.lee@gmail.com>
2025-01-13 14:49:07 +1100

    update adventuretube-cloud-redeploy.sh

--- adventuretube-cloud-redeploy.sh
+++ adventuretube-cloud-redeploy.sh
@@ -4,11 +4,16 @@
 echo "Cleaning and building Maven project..."
 ./mvnw clean package
 
-# Step 2: Build the Docker image
-echo "Building Docker image..."
-docker-compose -f docker compose -f docker-compose-clouds.yml build
+# Step 2: Run the Docker container
+echo "Starting the Docker container..."
+docker compose -f docker-compose-clouds.yml build
 
-# Step 3: Run the Docker container
+# Step 3: down the Docker image
+echo "Building Docker image..."
+docker compose -f docker-compose-clouds.yml down
+
+
+# Step 4: Run the Docker container
 echo "Starting the Docker container..."
 docker-compose -f docker compose -f docker-compose-clouds.yml up
 
