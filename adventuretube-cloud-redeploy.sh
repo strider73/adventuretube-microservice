75ea89e2b4ade506f608c81b2ea400b1458522b3
Chris Lee <strider.lee@gmail.com>
2025-01-13 14:57:12 +1100

    update command in correct way

--- adventuretube-cloud-redeploy.sh
+++ adventuretube-cloud-redeploy.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-# Step 1: Clean and build Maven project
+# Step 1: Pull the latest updates from the 'add-kafka' branch
+echo "Pulling latest updates from 'add-kafka' branch..."
+git checkout add-kafka
+git pull origin add-kafka
+
+# Step 2: Clean and build Maven project
 echo "Cleaning and building Maven project..."
 ./mvnw clean package
 
-# Step 2: Run the Docker container
-echo "Starting the Docker container..."
-docker compose -f docker-compose-clouds.yml build
-
-# Step 3: down the Docker image
+# Step 3: Build the Docker image
 echo "Building Docker image..."
-docker compose -f docker-compose-clouds.yml down
+docker-compose -f docker-compose-clouds.yml build
 
+# Step 4: Stop and remove existing Docker containers (if any)
+echo "Stopping and removing existing Docker containers..."
+docker-compose -f docker-compose-clouds.yml down
 
-# Step 4: Run the Docker container
+# Step 5: Start the Docker container with the new image
 echo "Starting the Docker container..."
-docker-compose -f docker compose -f docker-compose-clouds.yml up
+docker-compose -f docker-compose-clouds.yml up -d
 
 echo "Build and deployment completed successfully!"
