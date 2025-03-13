d13e027ee70c66ecd57791e3e388d44bef8e5e51
strider73 <strider.lee@gmail.com>
2025-03-13 14:49:00 +1100

    test build

--- config-service/src/main/java/com/adventuretube/ConfigServiceApplication.java
+++ config-service/src/main/java/com/adventuretube/ConfigServiceApplication.java
@@ -8,7 +8,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServiceApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(ConfigServiceApplication.class);
     }
+
+
 }
